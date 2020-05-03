0c7f3e6018e129adf8a3a7fa70c59c41b62dfe1b
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 16:44:07 +0700

    TUGAS 14 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas14.js
@@ -0,0 +1,15 @@
+
+var x = 37;
+var y = 14;
+var pembagian = x/y;
+
+console.log(Math.ceil(pembagian))
+console.log(Math.floor(pembagian))
+console.log(Math.round(pembagian))
+
+
+
+// C:\Users\USER\Documents\JS\javascript>node tugas14.js
+// 3
+// 2
+// 3
